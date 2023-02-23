6d004d33ded1066a61ced1363e6b15a10da6a0ae
Elnur Mammadov <elnur.mammadov@sectigo.com>
2023-02-23 17:18:30 +0400

    install only libfuse

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt remove neovim
-sudo apt install -y git fuse libfuse2
+sudo apt install -y git libfuse2
 
 # install latest nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
