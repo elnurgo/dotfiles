77aeca35f652daded8957e75e5d1b22a4c15b2bf
Elnur Mammadov <elnur.mammadov@sectigo.com>
2023-02-23 13:55:47 +0000

    fix ubuntu package names

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
 sudo apt update
 sudo apt upgrade -y
 sudo apt remove neovim
-sudo apt install -y git fuse2 libfuse2
+sudo apt install -y git fuse libfuse2
 
 # install latest nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
