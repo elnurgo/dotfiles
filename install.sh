399a5d85a5d871367a7e157515b72c6626440b11
Elnur Mammadov <elnur.mammadov@sectigo.com>
2023-02-23 14:51:39 +0000

    make autoload plug vim in proper place

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ sudo apt install -y git fuse libfuse2
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 
-mkdir -p ~/.local/bin/ ~/.config/nvim/site/
+mkdir -p ~/.local/bin/ ~/.local/share/nvim/site/autoload/
 mv nvim.appimage ~/.local/bin/nvim
 
-sh -c 'curl -fLo  ~/.config/nvim/site/plug.vim --create-dirs \
+sh -c 'curl -fLo  ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
