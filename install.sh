ec05bfd3326193cae34d51c29604a3b2c9d1633a
Elnur Mammadov <elnur.mammadov@sectigo.com>
2023-02-23 15:14:00 +0000

    fix

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.app
 chmod +x nvim
 sudo chown root:root nvim
 sudo chown root:root nvim
+sudo mv nvim /usr/bin
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/init.vim ~/.vimrc 
