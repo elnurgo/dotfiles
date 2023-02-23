1d46e933a3f4c131e9d32c6ad39e7692e24aa0d1
Elnur Mammadov <elnur.mammadov@sectigo.com>
2023-02-23 13:50:04 +0000

    create local bin folder

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ sudo apt install -y git fuse2 libfuse2
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 
+mkdir -p ~/.local/bin/
 mv nvim.appimage ~/.local/bin/nvim
 mv init.vim ~/init.vim
