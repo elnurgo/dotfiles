cc59111e6702768f762bfdba046f5b0d9444d32a
Elnur Mammadov <elnur.mammadov@sectigo.com>
2023-02-24 10:07:48 +0000

    init vim file format update for vim files

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt remove neovim
-sudo apt install -y git fuse libfuse2
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+sudo apt install -y python3.9 git fuse libfuse2 pip3 software-properties-common
 
 # install latest nvim
 wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
 chmod +x nvim
 sudo chown root:root nvim
-sudo chown root:root nvim
 sudo mv nvim /usr/bin
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p ~/.config/nvim/site/autoload/
+mkdir -p ~/.config/nvim/site/autoload/ ~/.nvim/plugged
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/init.vim ~/.config/nvim/init.vim 
