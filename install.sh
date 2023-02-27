f9240ddc5deb9664d9c6460e2645a4f9daf021cc
Elnur Mammadov <elnur.mammadov@sectigo.com>
2023-02-27 13:45:20 +0000

    remove duplicate plug vim installation

--- install.sh
+++ install.sh
@@ -12,8 +12,7 @@ wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.app
 chmod +x nvim
 sudo chown root:root nvim
 sudo mv nvim /usr/bin
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p ~/.config/nvim/site/autoload/ ~/.nvim/plugged
+mkdir -p ~/.config/nvim/autoload/ ~/.nvim/plugged/
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/init.vim ~/.config/nvim/init.vim 
